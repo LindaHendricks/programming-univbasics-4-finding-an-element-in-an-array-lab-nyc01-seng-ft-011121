088302f1694d37da850b766ad3ffaa55152b4070
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-18 00:04:30 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,8 +3,7 @@ def find_element_index(array, value_to_find)
  
  while counter < array.length do 
    if array[counter] == value_to_find
+     counter += 1 
      return counter
-    end 
-    counter += 1 
-   end 
+    end
 end
